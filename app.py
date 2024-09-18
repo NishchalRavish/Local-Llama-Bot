34caef569540ceb7a8df93a32972d7765bf0c3a4
Nishchal Ravish <ghanteravish@gmail.com>
2024-09-18 00:08:34 -0500

    Logo

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def chat(messages):
     message = {"role": "assistant", "content": output}
     return message
 
-st.title('llama 3 - 8B Bot')
+st.title('🦙 llama 3 - 8B Bot')
 st.write('Interact with llama 3 locally!')
 
 if "messages" not in st.session_state:
@@ -52,45 +52,3 @@ if prompt := st.chat_input("How can I help you Nishchal?"):
 
 if st.button("Clear Chat History"):
     st.session_state.messages = []
-
-
-
-
-
-
-
-
-
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-    
-# def get_bot_response(user_message):
-#     url = "http://localhost:11434/chat"
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "model": "llama3",
-#         "prompt": user_message
-#     }
-    
-#     response = requests.post(url, json=payload, headers=headers)
-    
-#     if response.status_code == 200:
-#         return response.json().get("response")
-#     else:
-#         return "Error: Unable to Connect to Bot"
-
-# for message in st.session_state.messages:
-#     if message['role'] == "user":
-#         st.markdown(f"**You:** {message['content']}")
-#     else:
-#         st.markdown(f"**Bot:** {message['content']}")
-
-# user_input = st.text_input("You", key="user_input")
-
-# if st.button("Send") and user_input:
-#     st.session_state.messages.append({"role":"user","content":user_input})
-#     response = get_bot_response(user_input)
-#     st.session_state.messages.append({"role":"bot","content":response})
-#     st.write(st.session_state)
-    
-#     st.session_state.user_input=""
