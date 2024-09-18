0bf2b21c636f71b4c5abb0bb26b032d897eb46db
Nishchal Ravish <ghanteravish@gmail.com>
2024-09-18 00:32:53 -0500

    Fix for accidental delete of session state

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ with st.sidebar:
     ''')
     st.markdown("### Made by Nishchal")
     
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
